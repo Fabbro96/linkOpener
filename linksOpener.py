76caa6daa3efea7379b9ec79562486a189915dc4
Fabrizio <46193809+Fabbro96@users.noreply.github.com>
2022-04-28 12:18:29 +0200

    Update and rename linkCalcio.py to linksOpener.py

--- linksOpener.py
+++ linksOpener.py
@@ -1,19 +1,19 @@
 import webbrowser
 
 i = 0
-file = open(r'C:\Users\Kerah\Desktop\links.txt', 'r')
+file = open(r'links.txt', 'r')
 stringhe = ''
 for x in file:
     i = x.rfind(' ')
     stringhe += x[:i]
     stringhe += "\n"
 
-scrivi = open(r'C:\Users\Kerah\Desktop\links.txt', 'w')
+scrivi = open(r'links.txt', 'w')
 scrivi.write(stringhe)
 scrivi.close()
 file.close()
 
-links = open(r'C:\Users\Kerah\Desktop\links.txt', 'r')
+links = open(r'links.txt', 'r')
 for x in links:
     print("Link: ", x)
     pth = "C:\Program Files\Google\Chrome\Application\chrome.exe"
